92f476af9cb03a65f8f89564dc1ab007e04896c3
Gyuyeong <baudaegam@gmail.com>
2023-11-03 11:54:24 +0900

    deleted some files

--- types.h
+++ types.h
@@ -12,4 +12,10 @@ typedef struct {
     struct client_t* next;  // next pointer
 } client_t;
 
+typedef struct {
+    int room_id;  // room id, unique
+    client_t* head;  // linked list of clients, points to the head
+    int client_count;  // number of clients in the room
+} room_t;
+
 #endif
